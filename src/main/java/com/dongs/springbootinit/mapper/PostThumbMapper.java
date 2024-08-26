bef188a80cae7be2ea92f057f1847cdd994579ba
dongs <764534336@qq.com>
2024-08-26 19:13:48 +0800

    create:基于spring boot的初始化项目

---
+++ src/main/java/com/dongs/springbootinit/mapper/PostThumbMapper.java
@@ -0,0 +1,17 @@
+package com.dongs.springbootinit.mapper;
+
+import com.dongs.springbootinit.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/dongs/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.dongs.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dongs.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
